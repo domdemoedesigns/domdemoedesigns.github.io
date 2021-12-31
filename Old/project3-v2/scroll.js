b205bc0ac43584c509b7f1dac40e6676f62abc0d
Dom <ddemoe@nd.edu>
2021-12-31 16:36:46 -0500

    Updated portfolio
    Latest portfolio including relevant projects from the past and placing old content into a folder together

---
+++ Old/project3-v2/scroll.js
@@ -0,0 +1,7 @@
+// Init Skrollr
+var s = skrollr.init({
+    forceHeight: false
+});
+ 
+// Refresh Skrollr after resizing our sections
+s.refresh($('.homeSlide'));
--- project3/scroll.js
+++
@@ -1,7 +0,0 @@
-// Init Skrollr
-var s = skrollr.init({
-    forceHeight: false
-});
- 
-// Refresh Skrollr after resizing our sections
-s.refresh($('.homeSlide'));
